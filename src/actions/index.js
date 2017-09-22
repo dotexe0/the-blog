5df7217918c969eeac5fde42305bdde2af3ef740
dotexe0 <dotexe0@gmail.com>
2017-09-21 18:54:57 -0700

    posts get request with action/reducer working

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ const BASE_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=IMTHEONEWHOKNOCKS'
 
 export const FETCH_POSTS = 'fetch_posts';
+
 export function fetchPosts() {
     const request = axios.get(`${BASE_URL}/posts${API_KEY}`);
     return {
