4ef3710a9f4278e52008ffa6e52cb826215e600c
Daniel Varela <dotexe0@gmail.com>
2017-09-21 20:45:56 -0700

    added Links back and forth between components

--- src/components/new-posts.js
+++ src/components/new-posts.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class NewPost extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/">
+            Back
+          </Link>
+        </div>
         New Posts
       </div>
     )
